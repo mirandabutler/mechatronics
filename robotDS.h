b57f702c445feb1a342638275b02ce7fd46ff75b
miranda <miranda.butler@colorado.edu>
2016-04-11 09:47:02 -0600

    added motor classes

--- robotDS.h
+++ robotDS.h
@@ -3,13 +3,27 @@
 
 #include "mbed.h"
 
-class Flasher {
+class motor1 {
 public:
-    Flasher(PinName pin);
-    void flash(int n);
+    motor1(PinName pin1, PinName pin2, PinName pin3);
+    void run1(pin1, pin2, pin3);
 
 private:
-    DigitalOut _pin;
+    DigitalOut _pin1; //direction
+    DigitalOut _pin2; //sleep
+    PwmOut _pin3; //PWM
+
+};
+
+class motor2 {
+public:
+    motor2(PinName pin1, PinName pin2);
+    void run2(pin1, pin2);
+
+private:
+    DigitalOut _pin1; //direction
+    PwmOut _pin2; //PWM
+
 };
 
 #endif
